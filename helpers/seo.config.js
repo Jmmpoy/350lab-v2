8230414f7b545dfbe87c21b0d086b52303c993d0
Jmmpoy <dunker35@hotmail.fr>
2023-03-29 11:56:28 +0200

    SEO & icons

--- helpers/seo.config.js
+++ helpers/seo.config.js
@@ -1,13 +1,13 @@
 export default {
-  defaultTitle: "350Lab",
+  defaultTitle: "350Lab© Studio - Brand, Direction & Development",
   description:
     "350Lab is a creative studio with design and development expertise. We partner with brands by developing solutions through strategy & design.",
-  titleTemplate: "350lab",
+  titleTemplate: "350LAB© Studio - Brand, Direction & Development",
   openGraph: {
     type: "website",
     locale: "en_GB",
     url: "https://350lab.com/",
-    site_name: "350Lab",
+    site_name: "350Lab© Studio - Brand, Direction & Development",
     images: [
       {
         url: "/public/opengraph.png",
