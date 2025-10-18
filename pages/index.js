c0cecc5d8fbf252e8cb15d7cbe41b98802d667d7
Jean-Marc <dunker35@hotmail.fr>
2025-10-18 04:07:54 +0200

    fix intro

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,12 @@
-import Layout from "@/components/layout";
-import { LazyMotion, domAnimation, m } from "framer-motion";
-import { NextSeo } from "next-seo";
-import Intro from "@/components/intro";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return (
-    <Layout class="relative no-scrollbar">
-      <NextSeo
-        title="350lab"
-        description="350Lab is a creative studio with design and development expertise.
-              We partner with brands by developing solutions through strategy &
-              design."
-      />
-      <LazyMotion features={domAnimation}>
-        <m.div initial="initial" animate="enter" exit="exit">
-          <Intro/>
-        </m.div>
-      </LazyMotion>
-    </Layout>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
+  return null;
 }
